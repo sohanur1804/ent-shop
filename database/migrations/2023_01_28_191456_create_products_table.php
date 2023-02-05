71831b8be84d6f525a61036aef7f32cf5f660893
Sohanur Rahman <sohanur1804@gmail.com>
2023-02-06 03:52:28 +0800

    updt

---
+++ database/migrations/2023_01_28_191456_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('price');
+            $table->unsignedBigInteger('stock');
+            $table->unsignedBigInteger('price_code');
+            $table->string('warrenty');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
---
+++ resources/views/product/products.blade.php
@@ -0,0 +1,15 @@
+<x-app-layout>
+    <div class="container mx-auto my-10 bg-white py-10 overflow-hidden shadow-sm sm:rounded-lg px-8">
+        <div class="flex">
+            <div class="flex-1">
+                <p>All Product Count:- </p>
+            </div>
+            <div class="flex-1">
+                <a href="" class="bg-blue-500 p-4 rounded text-white hover:bg-blue-400 " >Add New Product</a>
+            </div>
+            <div class="flex-1">
+                <a href="" class="bg-blue-500 p-4 rounded text-white hover:bg-blue-400 " >Add Existing Product</a>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
